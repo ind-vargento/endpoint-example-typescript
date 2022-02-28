64af5daaf8e3f175d72959974767406de112b480
Victor Argento <vargento@sklum.com>
2022-02-28 08:21:42 +0100

    feat: change body

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const port = 4000; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" );
+    res.send( "Hello from change-body branch" );
     console.log(new Date(), "{app: hello, visitor: victorargento}")
 } );
 
